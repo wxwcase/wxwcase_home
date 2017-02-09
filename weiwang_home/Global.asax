e253a4ca9cb6f23435072c701d734ca35d5128c1
wxwcase <weicwru@gmail.com>
2017-02-09 17:56:12 -0600

    Add project files.

---
+++ weiwang_home/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="weiwang_home.MvcApplication" Language="C#" %>
---
+++ weiwang_home/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(weiwang_home.Startup))]
+namespace weiwang_home
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
